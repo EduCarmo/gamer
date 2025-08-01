8beaba40e3bee4fae8e2a751e6866a461b8319e4
Eduardo Carmo <eduardocarmo72@gmail.com>
2025-07-31 22:55:23 -0300

    feat(core): metodo moeda brasileira

---
+++ packages/core/src/utils/Moeda.ts
@@ -0,0 +1,14 @@
+export default class Moeda {
+  static formatar(
+    valor: number,
+    localizacao: string = "pt-BR",
+    moeda: string = "BRL"
+  ): string {
+    const valorFinal = (valor ?? 0).toLocaleString(localizacao, {
+      style: "currency",
+      currency: moeda,
+    });
+
+    return valorFinal;
+  }
+}
