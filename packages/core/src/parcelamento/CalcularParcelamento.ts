2e81d402a7e94598ff0c575f6b9fb0a9a50ff36a
Eduardo Carmo <eduardocarmo72@gmail.com>
2025-08-02 12:25:45 -0300

    feat(core): adicionando logica de parcelamento

---
+++ packages/core/src/parcelamento/CalcularParcelamento.ts
@@ -0,0 +1,38 @@
+import { QTDE_MAX_PARCELAS, TAXA_JUROS_MENSAL } from "../../constants";
+import Parcelamento from "./Parcelamento";
+
+export default class CalcularParcelamento {
+  executar(
+    valor: number,
+    qtdParcelas: number = QTDE_MAX_PARCELAS,
+    taxaJuros: number = TAXA_JUROS_MENSAL
+  ): Parcelamento {
+    if (qtdParcelas < 2 || qtdParcelas > QTDE_MAX_PARCELAS) {
+      throw new Error(`Quantidade de parcelas deve estar entre 2 e ${QTDE_MAX_PARCELAS}`);
+    }
+
+    const totalComJuros = this.calcularJurosCompostos(
+      valor,
+      taxaJuros,
+      qtdParcelas
+    );
+    return {
+      qtdParcelas,
+      taxaJuros,
+      valorTotal: this.comDuasCasasDecimais(totalComJuros),
+      valorParcelas: this.comDuasCasasDecimais(totalComJuros / qtdParcelas),
+    };
+  }
+
+  private calcularJurosCompostos(
+    valorTotal: number,
+    taxaMensal: number,
+    qtdParcelas: number
+  ) {
+    return valorTotal * Math.pow(1 + taxaMensal, qtdParcelas);
+  }
+
+  private comDuasCasasDecimais(valor: number): number {
+    return Math.round(valor * 100) / 100;
+  }
+}
---
+++ packages/core/src/parcelamento/Parcelamento.ts
@@ -0,0 +1,6 @@
+export default interface Parcelamento {
+    valorTotal: number;
+    valorParcelas: number;
+    qtdParcelas: number;
+    taxaJuros: number;
+}
